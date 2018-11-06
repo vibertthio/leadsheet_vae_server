12ccb12369ac29e3830a64968f4b59d5d669bd1a
Hao-Min Liu (Paul) <liuhaumin@gmail.com>
2018-11-06 13:26:12 +0800

    Update client.py

--- v1/client.py
+++ v1/client.py
@@ -3,26 +3,38 @@ import numpy as np
 import json
 
 '''
-show drum roll in console
+show leadsheet sequence in console
 '''
-def printleadsheet(roll):
-    trans = np.flip(np.transpose(roll), 0)
+def printchord_seq(seq):
+    trans = np.flip(seq, 0)
     for r_i, r in enumerate(trans):
-        print('[{}]'.format(127 - r_i), end='') 
-        for i, w in enumerate(r):
-            if i > 0 and i % 12 == 0:
-                print('|', end='')
-            if w == 0:
-                print('_', end='')
-            else:
-                print('*', end='')
+        print('[{}]'.format(r_i), end='')
+        print() 
+        print('[{}]'.format(r), end='') 
         print()
+        # print('[{}]'.format(r_i), end='') 
+        # for i, w in enumerate(r):
+        #     if i > 0 and i % 12 == 0:
+        #         print('|', end='')
+        #     if w == 0:
+        #         print('_', end='')
+        #     else:
+        #         print('[{}]'.format(w), end='')
+        # print()
+
+def printmelody_seq(seq):
+    trans = np.flip(seq, 0)
+    for r_i, r in enumerate(trans):
+        print('[{}]'.format(r_i), end='')
+        print() 
+        print('[{}]'.format(r), end='') 
+        print() 
 
 addr = 'http://localhost:5003'
 SONG1 = '1'
 SONG2 = '3'
-# test_url = addr + '/static'
-test_url = addr + '/static' + '/'+SONG1+'/'+SONG2
+test_url = addr + '/static'
+# test_url = addr + '/static' + '/'+SONG1+'/'+SONG2
 content_type = 'application/json'
 headers = {'content-type': content_type}
 
@@ -33,18 +45,16 @@ response = requests.get(
 print(response)
 
 r_json = json.loads(response.text)
-melody_rolls = r_json['melody']
-chord_rolls = r_json['chord']
-print('length of melody: {}'.format(len(melody_rolls)))
-for i, d in enumerate(melody_rolls):
+chord_seq = r_json['chord']
+melody_seq = r_json['melody']
+for i, d in enumerate(chord_seq):
     x = i
     y = 0
     print('({}, {})'.format(x, y))
-    printleadsheet(d)
+    printchord_seq(d)
 
-print('length of chords: {}'.format(len(chord_rolls)))
-for i, d in enumerate(chord_rolls):
+for i, d in enumerate(melody_seq):
     x = i
     y = 0
     print('({}, {})'.format(x, y))
-    printleadsheet(d)
+    printmelody_seq(d)
